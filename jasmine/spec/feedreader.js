c29a1b16a19daa6b41e38b2c89f170eec8ff2881
Crystal Yungwirth <crystalyungwirth@gmail.com>
2018-07-28 00:49:37 -0400

    feat: Test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,7 +13,7 @@ $(function() {
    * a related set of tests. This suite is all about the RSS
    * feeds definitions, the allFeeds variable in our application.
    */
-  describe('RSS Feeds', function() {
+  describe('RSS Feeds', () => {
     /* This is our first test - it tests to make sure that the
      * allFeeds variable has been defined and that it is not
      * empty. Experiment with this before you get started on
@@ -22,7 +22,7 @@ $(function() {
      * page?
      * RSS Feeds test fails
      */
-    it('are defined', function() {
+    it('are defined', () => { //RSS Feeds are defined
       expect(allFeeds).toBeDefined();
       expect(allFeeds.length).not.toBe(0);
     });
@@ -32,9 +32,12 @@ $(function() {
      * in the allFeeds object and ensures it has a URL defined
      * and that the URL is not empty.
      */
-     it('urls are defined', function() {
-
-     });
+    it('URLs are defined and are not empty', function() { //RSS Feeds URLs are defined and are not empty
+      allFeeds.forEach((test) => {
+        expect(test.url).toBeDefined();
+        expect(test.url.length).not.toBe(0);
+      });
+    });
 
     /* TODO: Write a test that loops through each feed
      * in the allFeeds object and ensures it has a name defined
@@ -44,39 +47,39 @@ $(function() {
 
 
   /* TODO: Write a new test suite named "The menu" */
-  describe('The menu', function() {
+  //  describe('The menu', () => {
 
-    /* TODO: Write a test that ensures the menu element is
-     * hidden by default. You'll have to analyze the HTML and
-     * the CSS to determine how we're performing the
-     * hiding/showing of the menu element.
-     */
+  /* TODO: Write a test that ensures the menu element is
+   * hidden by default. You'll have to analyze the HTML and
+   * the CSS to determine how we're performing the
+   * hiding/showing of the menu element.
+   */
 
-    /* TODO: Write a test that ensures the menu changes
-     * visibility when the menu icon is clicked. This test
-     * should have two expectations: does the menu display when
-     * clicked and does it hide when clicked again.
-     */
+  /* TODO: Write a test that ensures the menu changes
+   * visibility when the menu icon is clicked. This test
+   * should have two expectations: does the menu display when
+   * clicked and does it hide when clicked again.
+   */
 
-  });
+  //  });
   /* TODO: Write a new test suite named "Initial Entries" */
-  describe('Initial Entries', function() {
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
+  //  describe('Initial Entries', () => {
+  /* TODO: Write a test that ensures when the loadFeed
+   * function is called and completes its work, there is at least
 
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+   * a single .entry element within the .feed container.
+   * Remember, loadFeed() is asynchronous so this test will require
+   * the use of Jasmine's beforeEach and asynchronous done() function.
+   */
 
-  });
+  //  });
 
   /* TODO: Write a new test suite named "New Feed Selection" */
-  describe('New Feed Selection', function() {
-
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
-  });
+  // describe('New Feed Selection', () => {
+  //
+  //   /* TODO: Write a test that ensures when a new feed is loaded
+  //    * by the loadFeed function that the content actually changes.
+  //    * Remember, loadFeed() is asynchronous.
+  //    */
+  // });
 }());
